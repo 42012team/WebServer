039775c4d18f6d960f15a471b244360c3a10e51b
Adaline <adelka1009@mail.ru>
2017-02-08 19:49:14 +0400

    auto-commit

---
+++ src/classes/configuration/Initialization.java
@@ -0,0 +1,62 @@
+package classes.configuration;
+
+import classes.activator.Activator;
+import classes.controllers.WebController;
+import classes.idgenerator.IdGenerator;
+import classes.idgenerator.IdGeneratorSingletonDB;
+import classes.model.behavior.managers.ActiveServiceManager;
+import classes.model.behavior.managers.ServiceManager;
+import classes.model.behavior.managers.UserManager;
+import classes.model.behavior.storages.impl.DBActiveServiceStorage;
+import classes.model.behavior.storages.impl.DBServiceStorage;
+import classes.model.behavior.storages.impl.DBUserStorage;
+import classes.processors.Configuration;
+import classes.processors.ConfigurationXML;
+import classes.processors.Initializer;
+import classes.processors.RequestProcessor;
+
+import java.util.Map;
+
+/**
+ * Created by User on 06.02.2017.
+ */
+public class Initialization {
+
+    private static volatile Initialization instance = null;
+    WebController controller=null;
+    private Initialization() {
+        IdGenerator idGenerator = IdGeneratorSingletonDB.getInstance();
+        UserManager userManager = new UserManager(new DBUserStorage(), idGenerator);
+        ServiceManager  serviceManager = new ServiceManager(new DBServiceStorage(), idGenerator);
+        ActiveServiceManager   activeServiceManager = new ActiveServiceManager(new DBActiveServiceStorage(),
+                idGenerator, serviceManager);
+        serviceManager.setActiveServiceManager(activeServiceManager);
+        Initializer initializer = new Initializer(userManager, activeServiceManager, serviceManager, "optimistic");
+        Configuration c = new ConfigurationXML();
+        Map<String, RequestProcessor> map = c.getMap(initializer);
+        controller = new WebController(map);
+        Activator activator = new Activator();
+        activeServiceManager.setActivator(activator);
+        activator.setActiveServiceManager(activeServiceManager);
+        activator.start();
+
+    }
+
+    public static Initialization getInstance() {
+        if (instance == null) {
+            synchronized (Initialization.class) {
+                if (instance == null) {
+                    instance = new Initialization();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public WebController initialization() {
+        synchronized (instance) {
+            return controller;
+        }
+    }
+
+}
