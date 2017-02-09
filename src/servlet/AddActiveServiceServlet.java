3704a93f3f78907a40575bbebe155c97c70b554b
Adaline <adelka1009@mail.ru>
2017-02-09 19:56:29 +0400

    auto-commit

--- src/servlet/AddActiveServiceServlet.java
+++ src/servlet/AddActiveServiceServlet.java
@@ -2,12 +2,18 @@ package servlet;
 
 import classes.configuration.Initialization;
 import classes.controllers.WebController;
+import classes.model.ActiveServiceStatus;
+import classes.model.User;
+import classes.request.impl.TransmittedActiveServiceParams;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by User on 09.02.2017.
@@ -20,7 +26,26 @@ public class AddActiveServiceServlet extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       User user= (User) request.getSession(true).getAttribute("user");
+       String dateToString=request.getParameter("activateDate");
+        System.out.println(dateToString+"mrr");
+                SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Date newDate = null;
+        try {
+            newDate = format.parse(dateToString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
 
+        TransmittedActiveServiceParams activeServiceParams = TransmittedActiveServiceParams.create()
+                .withServiceId(Integer.parseInt(request.getParameter("serviceId")))
+                .withUserId(user.getId())
+                .withDate(newDate)
+                .withCurrentStatus(ActiveServiceStatus.PLANNED)
+                .withNewStatus(ActiveServiceStatus.ACTIVE)
+                .withRequestType("createActiveService");
+        controller.indentifyObject(activeServiceParams);
+        request.getRequestDispatcher("/ShowActiveServicesPage").forward(request, response);
     }
 }
--- web/allowedToConnectServicesPage.jsp
+++ web/allowedToConnectServicesPage.jsp
@@ -1,5 +1,6 @@
 <%@ page import="java.util.List" %>
-<%@ page import="classes.model.Service" %><%--
+<%@ page import="classes.model.Service" %>
+<%@ page import="classes.model.User" %><%--
   Created by IntelliJ IDEA.
   User: User
   Date: 09.02.2017
@@ -12,8 +13,10 @@
     <title>Title</title>
 </head>
 <body>
+<form action="AddActiveServiceServlet" method="post">
 <% List<Service> serviceList= (List<Service>) request.getAttribute("allowedToConnectServices");
     int j=1;
+    User user= (User) session.getAttribute("user");
     for (Service s : serviceList) {%>
        <p>_______________________________________________________</p>
        <p>Услуга номер <%=j%></p>
@@ -21,9 +24,13 @@
         <p>Описание услуги:<%=s.getDescription()%></p>
         <p>Тип услуги:<%=s.getType()%>
        <p>_______________________________________________________</p>
-
+        <input type="hidden" name="serviceId" value="<%=s.getId()%>"/>
+       <input type="hidden" name="userId" value="<%=user.getId()%>">
+        <input type="text" name="activateDate"/>
+        <input type="submit" name="addActiveService" value="Добавить"/>
       <%  j++;
     }
 %>
+    </form>
 </body>
 </html>
