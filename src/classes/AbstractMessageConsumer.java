8e9cbe6e22af501d4a48ff243dd086dd475b7bc9
Adaline <adelka1009@mail.ru>
2017-04-27 23:54:03 +0400

    auto-commit

--- src/classes/AbstractMessageConsumer.java
+++ src/classes/AbstractMessageConsumer.java
@@ -1,6 +1,7 @@
 package classes;
 
 
+import classes.transport.TransportActivator;
 import classes.transport.TransportServiceMessage;
 import classes.util.InMemoryStorage;
 
@@ -8,14 +9,29 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractMessageConsumer implements MessageListener {
+    Map<Integer, Object> pool = new HashMap<Integer, Object>();
 
     @Override
     public void onMessage(Message message) {
         TransportServiceMessage textMessage = null;
         try {
-            textMessage = (TransportServiceMessage) ((ObjectMessage) message).getObject();
+            if (((ObjectMessage) message).getObject() instanceof TransportServiceMessage) {
+                TransportServiceMessage transportServiceMessage = (TransportServiceMessage) ((ObjectMessage) message).getObject();
+                pool.get(transportServiceMessage.getActiveServiceId().intValue()).notifyAll();
+                pool.remove(transportServiceMessage.getActiveServiceId().intValue());
+                //    textMessage = (TransportServiceMessage) ((ObjectMessage) message).getObject();
+            } else {
+
+
+            }
+
         } catch (JMSException e) {
             e.printStackTrace();
         }
@@ -24,5 +40,11 @@ public abstract class AbstractMessageConsumer implements MessageListener {
 
     protected void putToStorage(String message) {
         InMemoryStorage.add(getClass().getName() + " - " + message);
+    }
+
+    public void putInPool(int id, Object object) {
+        synchronized (pool) {
+            pool.put(id, object);
+        }
     }
 }
